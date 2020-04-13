ef6879a59f10695738ac85d7d34ede54f91eb52c
Ford Neild <hanfordn35@gmail.com>
2020-04-13 15:00:13 -0400

    hello world

--- models.py
+++ models.py
@@ -86,8 +86,8 @@ class LambdaMeans(Model):
 
         my_iterations = 2
         my_iterations = iterations
-
         (n, num_features) = X.shape
+        origin = [0] * num_features
         if(self.lambda0 == 0):
             #calculate lambda 0 to be the standard deviation of the points
             totalDistanceFromMean = 0
@@ -101,6 +101,8 @@ class LambdaMeans(Model):
         for iteration in range(my_iterations):
             #clear the assignments to those clusters, we will reassign them now
             for i in range(len(self.mu_k)):
+                if len(clusterBins[i]) == 0:
+                    self.mu_k[i] = origin
                 clusterBins[i] = []
             # for each point, put it in a cluster bin
             for i, x_i in enumerate(X):
@@ -185,9 +187,39 @@ class StochasticKMeans(Model):
                 [num_examples, num_features].
         """
         # TODO: Implement this!
-        raise Exception("You must implement this method!")
+        # raise Exception("You must implement this method!")
+        (n, num_features) = X.shape
+        origin = [0] * num_features
+        if len(self.mu_k) == 1:
+            #make one cluter as mean of points
+            self.mu_k[0] = np.asfarray(X.mean(axis=0))[0]
+        else:
+            #place two centers at the max and min points
+            #min initialization
+            min_x_i = X[0].toarray()[0]
+            min_x_i_distance = self.distance(origin, X[0].toarray()[0])
+            #min initialization
+            max_x_i = min_x_i
+            max_x_i_distance = min_x_i_distance
+            for x_i in X:
+                x_i = x_i.toarray()[0]
+                x_i_distance = self.distance(x_i, origin)
+                if( x_i_distance < min_x_i_distance):
+                    min_x_i_distance = x_i_distance
+                    min_x_i = x_i
+                if(x_i_distance > max_x_i_distance):
+                    max_x_i_distance =x_i_distance
+                    max_x_i = x_i
+            self.mu_k[0] = min_x_i
+            self.mu_k[-1] = max_x_i
+            for i in range(1, len(self.mu_k)-1):
+                # for each remaining cluster, spread them out evenly
+                self.mu_k[i] = min_x_i + i*(max_x_i- min_x_i)/(len(self.mu_k) - 1)
+        
 
-
+    def distance(self, point1, point2):
+        return np.sum(((point1 - point2)**2))**(1/2)
+    
     def fit(self, *, X, iterations):
         """
         Fit the LambdaMeans model.
@@ -198,8 +230,58 @@ class StochasticKMeans(Model):
                 [num_examples, num_features].
             iterations: int giving number of clustering iterations
         """
+        self.initialize_clusters(X)
+        c = 2
+        (n, num_features) = X.shape
         # TODO: Implement this!
-        raise Exception("You must implement this method!")
+        # raise Exception("You must implement this method!")
+        clusterBins = [] #each cluster has a bin of points
+        for i in range(len(self.mu_k)):
+            clusterBins.append([]) #since we start with 1 cluster
+
+        for iteration in range(iterations):
+            #clear the assignments to those clusters, we will reassign them now
+            beta = (iteration+1)*c #TODO: verify if should add 1
+            for i in range(len(self.mu_k)):
+                if len(clusterBins[i]) == 0:
+                    self.mu_k[i] = [0] * num_features
+                clusterBins[i] = []
+            # for each point, put it in a cluster bin
+            for i, x_i in enumerate(X):
+                x_i = x_i.toarray()[0]
+                min_distance = self.distance(x_i, self.mu_k[0])
+                min_center_index = 0
+                cur_distance = 0
+                for center_index, center in enumerate(self.mu_k):
+                    cur_distance = self.distance(x_i, center)
+                    if cur_distance < min_distance:
+                        #new best cluster
+                        min_distance = cur_distance
+                        min_center_index = center_index
+                clusterBins[min_center_index].append(x_i) #actually put this point in a cluster
+            print('--------------------------number of clusters', len(self.mu_k),'--------------------------')
+            for cluster_index, cluster_points in enumerate(clusterBins):
+                running_total_top = [0] * num_features
+                running_total_bottom = 0
+                for i, x_i in enumerate(X):
+                    x_i = x_i.toarray()[0]
+                    # for this point find average distance to each cluster
+                    total_distance_to_clusters = 0
+                    for center in self.mu_k:
+                        total_distance_to_clusters+= self.distance(x_i, center)
+                    d_hat = total_distance_to_clusters/len(self.mu_k)
+                    top = np.exp((-beta *self.distance(x_i, self.mu_k[cluster_index]))/d_hat)
+                    bottom = 0
+                    for center in self.mu_k:
+                        bottom+= np.exp((-beta *self.distance(x_i, center))/d_hat)
+                    p_ik = top/bottom
+                    running_total_top+=p_ik*x_i
+                    running_total_bottom+=p_ik
+                self.mu_k[cluster_index] = running_total_top/running_total_bottom
+
+                print(len(cluster_points), "points in cluster", cluster_index)
+                # re assign the center to be the mean of the points in this cluster
+                # self.mu_k[cluster_index] = np.mean(cluster_points, axis=0)
 
 
     def predict(self, X):
@@ -214,4 +296,19 @@ class StochasticKMeans(Model):
         """
         X = self._fix_test_feats(X)
         # TODO: Implement this!
-        raise Exception("You must implement this method!")
+        X = self._fix_test_feats(X)
+        (n, num_features) = X.shape
+        y = [0] * n
+        for i, x_i in enumerate(X):
+                x_i = x_i.toarray()[0]
+                min_distance = self.distance(x_i, self.mu_k[0])
+                min_center_index = 0
+                cur_distance = 0
+                for center_index, center in enumerate(self.mu_k):
+                    cur_distance = self.distance(x_i, center)
+                    if cur_distance < min_distance:
+                        #new best cluster
+                        min_distance = cur_distance
+                        min_center_index = center_index
+                y[i]= min_center_index #actually put this point in a cluster
+        return y
--- run.sh
+++ run.sh
@@ -1,9 +1,30 @@
 #train
-python3 main.py --mode train --algorithm lambda_means --model-file speech.lambda_means.model --train-data ./datasets/speech.train
+# python3 main.py --mode train --algorithm lambda_means --model-file speech.lambda_means.model --train-data ./datasets/speech.dev
 #test
-python3 main.py --mode test --model-file speech.lambda_means.model --test-data ./datasets/speech.train --predictions-file speech.train.predictions
-#compute clusters
-python3 cluster_accuracy.py ./datasets/speech.test speech.test.predictions
+# python3 main.py --mode test --model-file speech.lambda_means.model --test-data ./datasets/speech.train --predictions-file speech.train.predictions
+# #compute clusters
+# python3 cluster_accuracy.py ./datasets/speech.test speech.test.predictions
 
 
-python3 main.py --mode train --algorithm stochastic_k_means --model-file speech.stochastic_k_means.model --train-data ./datasets/speech.train --number-of-clusters 1
+# python3 main.py --mode train --algorithm stochastic_k_means --model-file speech.stochastic_k_means.model --train-data ./datasets/speech.train --number-of-clusters 1
+#test
+# python3 main.py --mode test --model-file speech.stochastic_k_means.model --test-data ./datasets/speech.train --predictions-file speech.train.predictions
+
+
+
+# rm *.model 2> /dev/null
+# rm *.predictions 2> /dev/null
+# echo Training lambda means on speech
+# python3 main.py --mode train --algorithm lambda_means --model-file speech.lambda.model --train-data datasets/speech.train
+# python3 main.py --mode test --model-file speech.lambda.model --test-data datasets/speech.dev --predictions-file speech.dev.predictions
+# python3 cluster_accuracy.py datasets/speech.dev speech.dev.predictions
+
+
+rm *.model 2> /dev/null
+rm *.predictions 2> /dev/null
+echo Training stochastic_k means on speech
+python3 main.py --mode train --algorithm stochastic_k_means --model-file speech.stochastic_k.model --train-data datasets/speech.train
+python3 main.py --mode test --model-file speech.stochastic_k.model --test-data datasets/speech.dev --predictions-file speech.dev.predictions
+python3 cluster_accuracy.py datasets/speech.dev speech.dev.predictions
+
+
